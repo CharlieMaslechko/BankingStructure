7cbb4ef39b31a41d250605b27a3b9a469b675e9f
Charles Maslechko <37425513+CharlieMaslechko@users.noreply.github.com>
2019-01-20 14:32:35 -0500

    added login screen with username text field and layout constraints

--- Banking/src/MainBankingClass.java
+++ Banking/src/MainBankingClass.java
@@ -1,9 +1,10 @@
+import java.awt.*;
 
-public class MainBankingClass {
+public class MainBankingClass extends CustomUtilities {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		GUI loginScreen = new GUI();
+		
 	}
 
 }
